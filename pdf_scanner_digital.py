f87dc46ff834f5d540236aad3f2851d6431cf8ea
Thomas <thomasextrem0408@gmail.com>
2025-03-10 15:12:53 -0300

    Edesur added

--- pdf_scanner_digital.py
+++ pdf_scanner_digital.py
@@ -75,4 +75,27 @@ class Digital_Scanner:
 
         return info
 
-    
+    def extract_info_edesur(self):
+        info = {'Fecha de Vto': [None, False],
+                'Monto Total a Pagar': [None, False]}
+        
+        for page in self.pdf['pages']:
+            
+            fecha_Vto_pattern = re.search(r'Vencimiento:\s*(\d{2}/\d{2}/\d{4})', page['content'])
+            if fecha_Vto_pattern:
+                fecha_Vto = fecha_Vto_pattern.group(1)
+            else:
+                fecha_Vto = "No encontrada"
+                
+            monto_pagar_pattern = re.search(r'TOTAL:\s*\$\s*([\d,.]+)', page['content'])
+            monto_pagar = monto_pagar_pattern.group(1) if monto_pagar_pattern else "No encontrado"
+            
+            if fecha_Vto != "No encontrado" and not info['Fecha de Vto'][1]:
+                info['Fecha de Vto'][1] = True
+                info['Fecha de Vto'][0] = fecha_Vto
+
+            if monto_pagar != "No encontrado" and not info['Monto Total a Pagar'][1]:
+                info['Monto Total a Pagar'][1] = True
+                info['Monto Total a Pagar'][0] = monto_pagar
+        
+        return info    
